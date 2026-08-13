3c2e5a07a9c6472e1deee591ac7931b43bb22aae
vien-io <vienryomania@gmail.com>
2026-08-13 13:17:41 +0800

    mobile debug

--- src/components/MobileHeroScene.tsx
+++ src/components/MobileHeroScene.tsx
@@ -7,6 +7,10 @@ import Nebula from "./3D/Nebula";
 export const MobileHeroScene = () => {
   return (
     <div className="absolute inset-0">
+       {/* DEBUG */}
+      <div className="absolute top-20 left-4 z-50 rounded bg-red-500 px-3 py-1 text-sm font-bold text-white">
+        MOBILE SCENE
+      </div>
       <Canvas dpr={[1, 1.5]} gl={{ antialias: true, powerPreference: "low-power" }}>
         <MobileCameraRig />
 
